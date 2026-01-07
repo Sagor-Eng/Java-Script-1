14cca8353c01e04db4cae83faae630f3e359f459
Sagor Hossen <hossenmdsagor8@gmail.com>
2026-01-08 01:40:30 +0600

    first commit

---
+++ js/Script.js
@@ -0,0 +1,102 @@
+// console.log("i am sagor..!");
+// alert("i am sagor")
+
+
+// var nam = "value"
+
+// var sagor = "hossen"
+// console.log(sagor);
+
+// var x = "20"
+// var x = "30"
+// console.log(x);
+
+// var x = "20"
+// var y = "30"
+// console.log(x);
+// console.log(y);
+
+// let x = 10
+// let x =20
+// console.log(x);
+
+// let x = 10
+// x =20
+// console.log(x);
+
+// const pi = 3.1416;
+// pi = 4.1416;
+// console.log(pi);
+
+// {
+//     var x = 10
+// }
+// console.log(x);
+
+// {
+//     let x = 10
+// }
+// console.log(x);
+
+// {
+//     let x = 10
+//     console.log(x);
+// }
+
+// let name ="sagor";
+// console.log("amar nam" + name);
+
+// let name ="sagor";
+// console.log("amar nam" + " " + name);
+
+// let nam = "pabna"
+// console.log(nam);
+
+// let location = "pabna"
+// console.log(location);
+
+// let area = "pabna"
+// console.log(area);
+
+// let loc = "sagor"
+// console.log(typeof loc);
+
+// let loc = 420
+// console.log(typeof loc);
+
+// let loc = true
+// console.log(typeof loc);
+
+// let numOne = 50
+// let numTwo = 20
+// console.log(numOne / numTwo);
+
+// let numOne = 50
+// let numTwo = 20
+// let result = numOne * numTwo
+// console.log(result);
+
+// 4 x 1 = 4
+let namtaNumber = 5
+console.log(namtaNumber + " " + "x" + " " + 1 + " " + " = " + namtaNumber*1);
+console.log(namtaNumber + " " + "x" + " " + 2 + " " + " = " + namtaNumber*2);
+console.log(namtaNumber + " " + "x" + " " + 3 + " " + " = " + namtaNumber*3);
+console.log(namtaNumber + " " + "x" + " " + 4 + " " + " = " + namtaNumber*4);
+console.log(namtaNumber + " " + "x" + " " + 5 + " " + " = " + namtaNumber*5);
+console.log(namtaNumber + " " + "x" + " " + 6 + " " + " = " + namtaNumber*6);
+console.log(namtaNumber + " " + "x" + " " + 7 + " " + " = " + namtaNumber*7);
+console.log(namtaNumber + " " + "x" + " " + 8 + " " + " = " + namtaNumber*8);
+console.log(namtaNumber + " " + "x" + " " + 9 + " " + " = " + namtaNumber*9);
+console.log(namtaNumber + " " + "x" + " " + 10 + " " + " = " + namtaNumber*10);
+
+
+
+
+
+
+
+
+
+
+
+
